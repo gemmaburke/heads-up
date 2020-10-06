765bcf46084c4065b058c42a8fd3bbfaee6db9d6
Neuza Sousa <neuzarssousa@gmail.com>
2020-10-06 14:59:43 +0200

    React router is not working

--- client/src/Components/Forum.js
+++ client/src/Components/Forum.js
@@ -4,7 +4,7 @@ class Forum extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello from Forum</h1>
+                <h1>Hello from Forum!</h1>
 
             </div>
         )
