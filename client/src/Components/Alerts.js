3790e99268887bba4a2ccf964ac93e67227a4ad7
Neuza Sousa <neuzarssousa@gmail.com>
2020-10-17 17:08:41 +0200

    Added an alert to Alerts

--- client/src/Components/Alerts.js
+++ client/src/Components/Alerts.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 
 class Alerts extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
+            phone_number: '',
             location: ''
         }
     }
@@ -25,24 +28,38 @@ class Alerts extends React.Component {
         this.setState({
             [name]: value
         });
-        console.log(this.state)
-    }
+        console.log(this.state);
+        alert("Your email, phone number and address have been successfully added to our database")
+        
+    };
+
+    
 
     render() {
         return (
-            <div>
+            <Container fluid>
                 <h2>Register for Alerts</h2>
                 <Form onSubmit={(e) => this.handleSubmit(e)}>
                     <Form.Group>
                         <Form.Label htmlFor="email">Insert Your Email-Address</Form.Label>
                             <Form.Control
+                                required
                                 name="email"
                                 type="email" 
                                 value={this.state.email}
                                 onChange={(e) => this.handleChange(e)}
                             />
+                        <Form.Label htmlFor="phone_number">Insert Your Phone Number</Form.Label>
+                            <Form.Control
+                                required
+                                name="phone_number"
+                                type="phone_number" 
+                                value={this.state.phone_number}
+                                onChange={(e) => this.handleChange(e)}
+                            />    
                         <Form.Label htmlFor="address">Insert Address</Form.Label>
-                            <Form.Control 
+                            <Form.Control
+                                required
                                 name="location"
                                 type="text"
                                 value={this.state.location} 
@@ -53,7 +70,7 @@ class Alerts extends React.Component {
                     </Form.Group>
                 </Form>
                 <br/>
-            </div>
+            </Container>
         )
     }
 }
